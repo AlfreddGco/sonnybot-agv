ef98982e3efe6bd40311a141afd12c8588bc06cc
Alfredo Garcia <alfredd.gco@gmail.com>
2022-11-10 23:17:38 -0600

    Threading fix for main file. Controllers are loading now

--- src/hoverboard-driver/src/main.cpp
+++ src/hoverboard-driver/src/main.cpp
@@ -7,30 +7,25 @@ int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("hoverboard_driver");
 
-    auto hoverboard = std::make_unique<Hoverboard>();
     auto rm = std::make_unique<hardware_interface::ResourceManager>();
-    rm->import_component(std::move(hoverboard));
+    rm->import_component(std::make_unique<Hoverboard>());
 
-    auto executor = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-    controller_manager::ControllerManager cm(
+    auto executor = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+    auto cm = std::make_shared<controller_manager::ControllerManager>(
         std::move(rm), executor, "hoverboard_manager");
+    executor->add_node(cm);
 
-    rclcpp::Time prev_time = node->now();
-    rclcpp::Rate rate(1.0);
-
-    while (rclcpp::ok()) {
-        const rclcpp::Time time = node->now();
-        const rclcpp::Duration period = time - prev_time;
-        prev_time = time;
-
-        cm.read();
-        cm.update();
-        cm.write();
-
-        rclcpp::spin_some(node);
-        rate.sleep();
-    }
-
-    RCLCPP_INFO(node->get_logger(), "While loop finished.");
+    // NOTE: Threading model could be improved.
+    auto updater = std::thread([&](void) -> void {
+        while(rclcpp::ok()){
+            cm->read();
+            cm->update();
+            cm->write();
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        }
+    });
+    executor->spin();
+    updater.join();
+    rclcpp::shutdown();
     return 0;
 }
